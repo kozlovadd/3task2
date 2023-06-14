464c43f4cc1ce536ca423577f322edb0f55497c0
kozlovadd <kdd_96@mail.ru>
2023-06-14 20:41:50 +0300

    committwo

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,9 +2,9 @@ public class BmiService {
     public int calculate(int bmi) {
         double weight = 58;
         double height = 1.68;
-        int weight_kg = (int) weight ;
+        int weight_kg = (int) weight;
         int height_m = (int) height;
-        int index = weight_kg / (height_m*height_m);
+        int index = weight_kg / (height_m * height_m);
         return index;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         BmiService service = new BmiService();
         double weight = 58;
         double height = 1.68;
-        int weight_kg = (int) weight ;
+        int weight_kg = (int) weight;
         int height_m = (int) height;
         int index = service.calculate(weight_kg);
         System.out.println(index);
